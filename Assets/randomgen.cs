61f7c8bc1b5a543f4b94bf43b4af4d4c494ee9cd
Tom <tomwilliamfranklin@gmail.com>
2020-04-27 22:51:59 +0100

    added block layer generation currently dirt and grass.

--- Assets/randomgen.cs
+++ Assets/randomgen.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections;
+﻿
+
+using Model;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class randomgen : MonoBehaviour
 {
     public GameObject currentBlockType;
-
-    public float amp = 10f;
+    public int chunkSize = 10;
+    public int worldSize = 10;
     public float freq = 10f;
-    public int worldHeight = 5;
+    public int amp = 10;
+    public int worldHeight = 10;
+    public int h = 0;
+
+    private Dictionary<string, GameObject> _Dictionary;
+    [SerializeField] List<string> _Keys;
+    [SerializeField] List<GameObject> _Values;
+
     // Start is calleaewdwaqdawdd before the first frame update
     void Start()
     {
-        int cols = 100;
-        int rows = 100;
+        int cols = chunkSize;
+        int rows = chunkSize;
+
+        _Dictionary = _Keys.Zip(_Values, (k, v) => new { k, v })
+              .ToDictionary(x => x.k, x => x.v);
 
         for (int x = 0; x < cols; x++)
         {
@@ -22,11 +35,18 @@ public class randomgen : MonoBehaviour
                 float y = Mathf.Round(Mathf.PerlinNoise(x/freq, z/freq) * amp);
 
                 for(int i = worldHeight + (int) y; i > 0; i--)
-                {   
-                    
-                    var newBlock = GameObject.Instantiate(currentBlockType);
+                {
+                    var newBlock = new GameObject();
+                    switch(i)
+                    {
+                        case var expression when i == worldHeight + (int) y:
+                            newBlock = GameObject.Instantiate(_Dictionary["grass"]);
+                            break;
+                        default:
+                            newBlock = GameObject.Instantiate(_Dictionary["dirt"]);
+                            break;
+                    }
                     newBlock.transform.position = new Vector3(x, i, z);
-
                 }
 
 
