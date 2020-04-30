4adf10d3661e573f7cf21bd0f04423ace2928001
Tom <tomwilliamfranklin@gmail.com>
2020-04-30 22:55:10 +0100

    Redone rendering. Will now render blocks in chunks for better performance. Renaming of files

---
+++ Model/CombinedGameObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Model
+{
+    public class CombinedGameObject
+    {
+        public string hello = "hello";
+        public CombineInstance combinedInstance = new CombineInstance();
+        public MeshFilter gameObject = new MeshFilter();
+    }
+}
---
+++ isVisible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isVisible : MonoBehaviour
+{
+    Renderer m_Renderer;
+    // Use this for initialization
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Renderer.isVisible)
+        {
+
+            GameObject.Destroy(this);
+            Debug.Log("Object is visible " + this.name.ToString());
+        }
+        else Debug.Log("Object is visible " + this.name.ToString());
+    }
+}
